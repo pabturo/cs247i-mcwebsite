24a30d850d7f7a39c2867509a93e14055d995cd7
pabturo <pablo.ocampo@mac.com>
2021-11-03 12:38:47 -0700

    Initial template

---
+++ js/mccustom.js
@@ -0,0 +1,45 @@
+// Offset for Site Navigation
+$('#siteNav').affix({
+	offset: {
+		top: 100
+	}
+})
+
+$(document).ready(function () {
+    // Handler for .ready() called.
+    $('html, body').animate({
+        scrollTop: $(window.location.hash).offset().top
+    }, 'slow');
+});
+
+function scrollTo(topicID) {
+	var container = $('#backgroundinfo');
+	var scrollTo = $(topicID);
+
+	// Calculating new position of scrollbar
+	var position = scrollTo.offset().top 
+			- container.offset().top 
+			+ container.scrollTop();
+
+	// Setting the value of scrollbar
+	$("#backgroundinfo").animate({
+		scrollTop: position
+	})
+
+}
+
+function scrollToCauses() {
+	scrollTo("#causes")
+}
+
+function scrollToSocial() {
+	scrollTo("#socialmedia")
+}
+
+function scrollToDunbar() {
+	scrollTo("#dunbar")
+}
+
+function scrollToFacts() {
+	scrollTo("#facts")
+}
